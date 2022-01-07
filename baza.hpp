b873003774dd96056c83da4d50b6aa0f04a2741d
cristian5600 <92854074+cristian5600@users.noreply.github.com>
2022-01-07 15:38:43 +0200

    Update baza.hpp

--- baza.hpp
+++ baza.hpp
@@ -15,9 +15,6 @@ public:
 	virtual void afisare() = 0;
 	virtual ~Baza() = 0;
 	
-	virtual bool iszona1() = 0;
-	virtual bool iszona2() = 0;
-	virtual bool iszona3() = 0;
 	virtual float prezenta_vot(int [],int [],int) = 0;
 	virtual float punctaj_nu(int [],int) = 0;
 	virtual float punctaj_da(int [],int) = 0;
